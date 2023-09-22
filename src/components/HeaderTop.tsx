d5c27b853764492db033a25f722bd2fcea7a5127
Matthieu96Code <117777066+Matthieu96Code@users.noreply.github.com>
2023-09-22 23:08:57 +0000

    Create HeaderTop.tsx

---
+++ src/components/HeaderTop.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const HeaderTop = () => {
+  return (
+    <div className="border-b border-gray-200 hidden sm:block">
+        <div className="contailner py-4"></div>
+    </div>
+  )
+}
+
+export default HeaderTop
