d288575a689c30b3b00b37eeb57cb4c9ab6d139a
Matthieu96Code <117777066+Matthieu96Code@users.noreply.github.com>
2023-09-27 09:18:09 +0000

    Add social media icons

--- src/components/HeaderTop.tsx
+++ src/components/HeaderTop.tsx
@@ -1,9 +1,31 @@
 import React from 'react'
 
+import {BsFacebook, BsTwitter, BsInstagram, BsLinkedin} from "react-icons/bs"
+
 const HeaderTop = () => {
   return (
     <div className="border-b border-gray-200 hidden sm:block">
-        <div className="contailner py-4"></div>
+        <div className="container py-4">
+          <div className="flex justify-between items-center">
+            <div className="hidden lg:flex gap-1">
+              <div className="header_top__icon_wrapper">
+                <BsFacebook />
+              </div>
+              <div className="header_top__icon_wrapper">
+                <BsTwitter />
+              </div>
+              <div className="header_top__icon_wrapper">
+                <BsInstagram />
+              </div>
+              <div className="header_top__icon_wrapper">
+                <BsLinkedin />
+              </div>
+            </div>
+            <div className="text-gray-500 text-[12px]">
+              <b>FREE SHIPPING</b> THIS WEEK ORDER OVER - $55
+            </div>
+          </div>
+        </div>
     </div>
   )
 }
